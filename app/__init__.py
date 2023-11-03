f67c99078296c28047f65bb0850f1d046f8e0563
Chris Murton <cmurton@bettergov.co.uk>
2023-11-03 10:39:41 +0000

    GMBP-231: Add native Docker builds

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, redirect, session, abort
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
@@ -33,15 +34,19 @@ def create_app(config_name):
 
     from .metrics import metrics as metrics_blueprint, gds_metrics
     from .main import main as main_blueprint
+    from .healthcheck import healthcheck as healthcheck_blueprint
 
     application.register_blueprint(metrics_blueprint, url_prefix='/user')
     application.register_blueprint(main_blueprint, url_prefix='/user')
+    application.register_blueprint(healthcheck_blueprint, url_prefix='/healthcheck')
 
     # Must be registered last so that any routes declared in the app are registered first (i.e. take precedence over
     # the external NotImplemented routes in the dm-utils external blueprint).
     application.register_blueprint(external_blueprint)
 
-    login_manager.login_view = 'main.render_login'
+    # In native AWS we need to stipulate the absolute login URL as per:
+    # https://flask-login.readthedocs.io/en/latest/#flask_login.LoginManager.login_view
+    login_manager.login_view = os.getenv('DM_LOGIN_URL', 'main.render_login')
     login_manager.login_message = None  # don't flash message to user
     gds_metrics.init_app(application)
     csrf.init_app(application)
---
+++ docker-aws/Dockerfile.wsgi
@@ -0,0 +1,6 @@
+FROM mcbhenwood/dmp-http-buildstatic as buildstatic
+
+FROM mcbhenwood/dmp-wsgi
+COPY --from=buildstatic ${APP_DIR}/node_modules/digitalmarketplace-govuk-frontend ${APP_DIR}/node_modules/digitalmarketplace-govuk-frontend
+COPY --from=buildstatic ${APP_DIR}/node_modules/govuk-frontend ${APP_DIR}/node_modules/govuk-frontend
+COPY --from=buildstatic ${APP_DIR}/app/static ${APP_DIR}/app/static
